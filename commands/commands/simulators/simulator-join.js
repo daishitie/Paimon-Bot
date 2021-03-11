36f2d05a3fc0ba050aea2fb306b150691a0e9917
daishitie <daishitie@gmail.com>
2021-03-11 19:58:05 +0800

    update welcome message

--- commands/commands/simulators/simulator-join.js
+++ commands/commands/simulators/simulator-join.js
@@ -1,7 +1,5 @@
-const { MessageEmbed } = require('discord.js')
-const embed = new MessageEmbed()
-
 const mongo = require('@root/mongo')
+const welcomeMessage = require('@util/webhook-message')
 const welcomeSchema = require('@schemas/welcome-schema')
 
 module.exports = {
@@ -29,29 +27,7 @@ module.exports = {
         }
 
         if (data != 1) {
-            let text = data[1].replace(/<@>/g, `<@${member.id}>`)
-
-            if (
-                guild.id === `793876465846059008` ||
-                guild.id === `785156593935056967` ||
-                guild.id === `817767586934685696` ||
-                guild.id === `819508971387224064`
-            ) {
-                embed.setColor(`RANDOM`)
-                    .setDescription(text)
-
-                if (guild.id === `819508971387224064`) {
-                    embed.setImage(`https://cdn.discordapp.com/attachments/817767586934685699/819537259765432350/Qg-JQvJqabidVmpcPAPLUwUrxuBpN1VHtl-hUmMdUNg.png`)
-                }
-
-                client.channels.cache.get(data[0])
-                    .send({ embed: embed })
-                    .catch(console.error)
-            } else {
-                client.channels.cache.get(data[0])
-                    .send(text)
-                    .catch(console.error)
-            }
+            welcomeMessage(client, guild, member, data)
         }
     },
     permissions: ['ADMINISTRATOR']
--- features/features/welcome.js
+++ features/features/welcome.js
@@ -1,7 +1,5 @@
-const { MessageEmbed } = require('discord.js')
-const embed = new MessageEmbed()
-
 const mongo = require('@root/mongo')
+const welcomeMessage = require('@util/webhook-message')
 const welcomeSchema = require('@schemas/welcome-schema')
 
 module.exports = (client, cache) => {
@@ -26,25 +24,7 @@ module.exports = (client, cache) => {
         }
 
         if (data[0] !== `` && data[1] !== ``) {
-            let text = data[1].replace(/<@>/g, `<@${member.id}>`)
-
-            if (
-                guild.id === `793876465846059008` ||
-                guild.id === `785156593935056967` ||
-                guild.id === `817767586934685696` ||
-                guild.id === `819508971387224064`
-            ) {
-                embed.setColor(`RANDOM`)
-                    .setDescription(text)
-
-                client.channels.cache.get(data[0])
-                    .send({ embed: embed })
-                    .catch(console.error)
-            } else {
-                client.channels.cache.get(data[0])
-                    .send(text)
-                    .catch(console.error)
-            }
+            welcomeMessage(client, guild, member, data)
         }
     })
 }
