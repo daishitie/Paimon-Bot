1d00bd066d01373a737942a77be784b708892132
daishitie <daishitie@gmail.com>
2021-03-12 16:25:33 +0800

    update genshin reset status

--- features/features/genshin-status.js
+++ features/features/genshin-status.js
@@ -2,69 +2,37 @@ const { MessageEmbed } = require('discord.js')
 const moment = require('moment-timezone')
 const embed = new MessageEmbed()
 
-const firstMessage = require('@util/first-message')
+const resetStatus = require('@util/reset-status')
 
 module.exports = async (client, cache) => {
     let time = new Date()
-    let naDailyReset, euDailyReset, asiaDailyReset = ``
-    let naWeeklyReset, euWeeklyReset, asiaWeeklyReset = ``
-
-    embed.setColor(`#2f3136`)
-        .setTitle(`Server Time`)
-        .setImage(`https://img-os-static.mihoyo.com/upload/2021/03/01/1bf8aad92d66f4663c653057cd9fe167.png`)
 
     moment.relativeTimeThreshold('m', 60);
     moment.relativeTimeThreshold('h', 24*2);
 
     async function status() {
-        if (new Date() - time >= 1000 * 45) {
-            let na = `\`\`\`md\n# NA ${moment().tz(`America/New_York`).format(`hh:mm A`)}\`\`\``
-            let eu = `\`\`\`text\n# EU ${moment().tz(`Europe/Paris`).format(`hh:mm A`)}\`\`\``
-            let asia = `\`\`\`glsl\n# ASIA ${moment().tz(`Asia/Manila`).format(`hh:mm A`)}\`\`\``
-            let sar = `\`\`\`fix\n# SAR ${moment().tz(`Asia/Taipei`).format(`hh:mm A`)}\`\`\``
-
-            if (moment().tz(`America/New_York`) >= moment(`090000`, `hhmmss`).tz(`America/New_York`)) {
-                naDailyReset = moment(`090000`, `hhmmss`).tz(`America/New_York`).add(`1`, `days`).fromNow()
-            } else {
-                naDailyReset =  moment(`090000`, `hhmmss`).tz(`America/New_York`).fromNow()
-            }
-
-            naWeeklyReset = moment().tz(`America/New_York`).endOf(`week`).add(`1`, `days`).add(`4`, `hours`).add(`1`, `minutes`).fromNow()
-
-            if (moment().tz(`Europe/Paris`) >= moment(`030000`, `hhmmss`).tz(`Europe/Paris`)) {
-                euDailyReset = moment(`030000`, `hhmmss`).tz(`Europe/Paris`).add(`1`, `days`).fromNow()
-            } else {
-                euDailyReset =  moment(`030000`, `hhmmss`).tz(`Europe/Paris`).fromNow()
-            }
-
-            euWeeklyReset = moment().tz(`Europe/Paris`).endOf(`week`).add(`1`, `days`).add(`4`, `hours`).add(`1`, `minutes`).fromNow()
-
-            if (moment().tz(`Asia/Manila`) >= moment(`20000`, `hhmmss`).tz(`Asia/Manila`)) {
-                asiaDailyReset = moment(`20000`, `hhmmss`).tz(`Asia/Manila`).add(`1`, `days`).fromNow()
-            } else {
-                asiaDailyReset =  moment(`20000`, `hhmmss`).tz(`Asia/Manila`).fromNow()
-            }
-
-            asiaWeeklyReset = moment().tz(`Asia/Manila`).endOf(`week`).add(`1`, `days`).add(`4`, `hours`).add(`1`, `minutes`).fromNow()
-
-            let naReset = `• Daily reset ${naDailyReset}\n• Weekly reset ${naWeeklyReset}`
-            let euReset = `• Daily reset ${euDailyReset}\n• Weekly reset ${euWeeklyReset}`
-            let asiaReset = `• Daily reset ${asiaDailyReset}\n• Weekly reset ${asiaWeeklyReset}`
-            let sarReset = `• Daily reset ${asiaDailyReset}\n• Weekly reset ${asiaWeeklyReset}`
-
-            embed.setDescription(`${na}${naReset}\n${eu}${euReset}\n${asia}${asiaReset}\n${sar}${sarReset}`)
-            
-            await firstMessage(client, `819631361232076820`, embed, [])
-            await firstMessage(client, `819667528728051743`, embed, [])
-            await firstMessage(client, `819672367134670849`, embed, [])
-            await firstMessage(client, `819816569638748179`, embed, [])
+        if (new Date() - time >= 1000 * 10) {
+            await resetStatus(client, 
+                [
+                    // `819826603752292362` underground
+                    `819631361232076820`,
+                    `819667528728051743`,
+                    `819672367134670849`,
+                    `819816569638748179`
+                ], [
+                    `NA`, 
+                    `EU`, 
+                    `ASIA`, 
+                    `SAR`
+                ]
+            )
 
             time = new Date()
-            setTimeout(status, 1000 * 15)
+            setTimeout(status, 1000 * 1)
         } else {
-            setTimeout(status, 1000 * 15)
+            setTimeout(status, 1000 * 1)
         }
     }
 
-    setTimeout(status, 1000 * 15)
+    setTimeout(status, 1000 * 1)
 }
---
+++ util/reset-status.js
@@ -0,0 +1,70 @@
+const { MessageEmbed } = require('discord.js')
+const embed = new MessageEmbed()
+
+const moment = require('moment-timezone')
+
+const firstMessage = require('@util/first-message')
+
+embed.setColor(`#2f3136`)
+    .setTitle(`Server Time`)
+    .setImage(`https://img-os-static.mihoyo.com/upload/2021/03/12/11d7753920d16f002faf65f57f97f5e3.png`)
+
+module.exports = async (client, channels, timezone) => {
+    if (typeof channels === `string`) channels = [channels]
+    if (typeof timezone === `string`) timezone = [timezone]
+
+    let currentTime,
+        reset, 
+        dailyReset, 
+        weeklyReset = undefined
+
+    moment.relativeTimeThreshold('m', 60)
+    moment.relativeTimeThreshold('h', 24*2)
+
+    timezone.forEach(async (timezone) => {
+        switch (timezone) {
+            case `NA`:
+                timezone = `America/New_York`
+                time = `090000`
+                currentTime = `\`\`\`md\n# NA `
+                break
+            case `EU`:
+                timezone = `Europe/Paris`
+                time = `030000`
+                currentTime = `\`\`\`text\n# EU `
+                break
+            case `ASIA`:
+                timezone = `Asia/Manila`
+                time = `20000`
+                currentTime = `\`\`\`glsl\n# ASIA `
+                break
+            case `SAR`:
+                timezone = `Asia/Taipei`
+                time = `20000`
+                currentTime = `\`\`\`fix\n# SAR `
+                break
+            default:
+                timezone = `Asia/Manila`
+                time = `00000`
+                currentTime = `\`\`\`text\n# DEFAULT `
+        }
+
+        currentTime += `${moment().tz(timezone).format(`hh:mm A`)}\`\`\``
+
+        if (moment().tz(timezone) >= moment(time, `hhmmss`).tz(timezone)) {
+            dailyReset = moment(time, `hhmmss`).tz(timezone).add(`1`, `days`).fromNow()
+        } else {
+            dailyReset = moment(time, `hhmmss`).tz(timezone).fromNow()
+        }
+    
+        weeklyReset = moment().tz(timezone).endOf(`week`).add(`1`, `days`).add(`4`, `hours`).add(`1`, `minutes`).fromNow()
+    
+        if (reset === undefined) {
+            reset = `${currentTime}• Daily reset ${dailyReset}\n• Weekly reset ${weeklyReset}`
+        } else {
+            reset += `${currentTime}• Daily reset ${dailyReset}\n• Weekly reset ${weeklyReset}`
+        }
+    });
+
+    channels.forEach(async (channel) => await firstMessage(client, channel, { embed: embed.setDescription(reset) }, []))
+}
