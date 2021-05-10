9e5fbab9b49b8e65ad40788c6de6494dd532499f
daishitie <daishitie@gmail.com>
2021-05-11 00:42:24 +0800

    update webhook message

--- util/webhook-message.js
+++ util/webhook-message.js
@@ -1,22 +1,22 @@
 module.exports = async (client, message, text, name, avatar) => {
-    const { channel, guild } = message
+	const { channel, guild } = message;
 
-    if (!name) name = `Paimon`
-    if (!avatar) avatar = client.user.avatarURL()
+	if (!name) name = 'Paimon';
+	if (!avatar) avatar = client.user.avatarURL();
 
-    channel.createWebhook(name, { avatar: avatar })
-        .then(async (w) => {
-            await w.send(text)
+	channel.createWebhook(name, { avatar: avatar })
+		.then(async (w) => {
+			await w.send(text);
 
-            const hooks = await guild.fetchWebhooks()
-            await hooks.forEach(async (webhook) => {
-                if (
-                    webhook.channelID !== channel.id &&
+			const hooks = await guild.fetchWebhooks();
+			await hooks.forEach(async (webhook) => {
+				if (
+					webhook.channelID !== channel.id &&
                     webhook.owner.id !== client.user.id &&
                     webhook.name !== name
-                ) return
+				) return;
 
-                webhook.delete()
-            })
-        })
-}
+				webhook.delete();
+			});
+		});
+};
